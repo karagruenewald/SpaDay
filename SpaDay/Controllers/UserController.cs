9ec511320eb0c85be04006bad256c6a1634a1fcd
Kara Gruenewald <karagruenewald@gmail.com>
2020-07-15 22:26:28 -0500

    finished ch14 studio and bonus

--- SpaDay/Controllers/UserController.cs
+++ SpaDay/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,11 +18,32 @@ namespace SpaDay.Controllers
         }
 
 
+        [Route("/user/add")]
         public IActionResult Add ()
         {
             return View();
         }
 
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            
+            if(newUser.Password == verify)
+            {
+                ViewBag.name = newUser.Username;
+                ViewBag.date = newUser.Date;
+                ViewBag.email = newUser.Email;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Your passwords do not match.";
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                return View("Add");
+            }
+        }
 
     }
 
--- SpaDay/Models/User.cs
+++ SpaDay/Models/User.cs
@@ -7,9 +7,18 @@ namespace SpaDay.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public DateTime Date { get; }
 
         public User()
         {
+            Date = DateTime.Now;
+        }
+
+        public User(string username, string email, string password): this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
         }
     }
 }
